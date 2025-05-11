64271355a83f8b50cf92d585637dac50e9cddc37
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-11 14:53:57 +0100

    first jsx, header, body and footer created

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,38 @@ const App = () => {
   return (
     <div className='container'>
       <div className="chatbot-popup">
+        {/* Chatbot Header */}
         <div className="chat-header">
           <div className="header-info">
             <ChatbotIcon />
             <h2 className="logo-text">Chatbot</h2>
           </div>
-          <span className='material-symbols-rounded'>keyboard_arrow_down</span>
+          <button className='material-symbols-rounded'>keyboard_arrow_down</button>
 
+        </div>
+        {/* chatbot body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text">
+              Hey there 👋 <br/> How can I help you?
+            </p>
+          </div>
+
+          <div className="message user-message">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptate.
+            </p>
+          </div>
+        </div>
+
+          {/* chatbot footer */}
+        <div className="chat-footer">
+          <form action="#" className="chat-form">
+            <input type="text" placeholder='Message...' className='message-input' required />
+            <button className='material-symbols-rounded'>arrow_upward</button>
+
+          </form>
         </div>
       </div>
     </div>
