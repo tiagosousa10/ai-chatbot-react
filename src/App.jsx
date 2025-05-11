7a1dcb501010cc2f07568b7cdee49fa3afc5b3e3
tiagosousa <tiagosousa.tams@hotmail.com>
2025-05-11 14:42:11 +0100

    first configurations to start the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className="chatbot-popup">
+        <div className="chat-header">
+          <div className="header-info">
+            <h2 className="logo-text">Chatbot</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
